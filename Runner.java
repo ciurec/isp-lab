e3d7b48f52d0e72b0c7e1f92431c041ce5aca635
ciurec <ciurean.catalin@outlook.com>
2025-01-28 22:44:26 +0200

    init

---
+++ Runner.java
@@ -0,0 +1,29 @@
+import lab2.*;
+
+public class Runner {
+    public static void main(String[] args) {
+     //  Lab 2
+        ExerciseOne exerciseOne = new ExerciseOne();
+        exerciseOne.run();
+
+
+        ExerciseTwo exerciseTwo = new ExerciseTwo();
+
+        exerciseTwo.run();
+
+        ExerciseThree exerciseThree = new ExerciseThree();
+        exerciseThree.run();
+        ExerciseFour exerciseFour = new ExerciseFour();
+        exerciseFour.run();
+
+        ExerciseFive exerciseFive = new ExerciseFive();
+        exerciseFive.run();
+
+        ExerciseSix exerciseSix = new ExerciseSix();
+        exerciseSix.run();
+
+        // Lab 3
+
+
+    }
+}
---
+++ lab2/utils/sort/QuickSort.java
@@ -0,0 +1,42 @@
+package lab2.utils.sort;
+
+public class QuickSort {
+
+    /* The main function that implements QuickSort()
+      a[] --> Array to be sorted,
+      l  --> Starting index,
+      h  --> Ending index */
+   public void sort(int a[], int l, int h) {
+        if (l < h) {
+            int pi = partition(a, l, h);
+
+            // Recursively sort elements before
+            // partition and after partition
+            sort(a, l, pi - 1);
+            sort(a, pi + 1, h);
+        }
+    }
+
+    private int partition(int a[], int low, int high) {
+        int pivot = a[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+
+            // If current element is smaller than or
+            // equal to pivot
+            if (a[j] <= pivot) {
+                i++;
+
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+
+        int temp = a[i + 1];
+        a[i + 1] = a[high];
+        a[high] = temp;
+
+        return i + 1;
+    }
+}
