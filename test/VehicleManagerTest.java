40d6752f6853922af1e3469ac055c012e295fee6
ciurec <ciurean.catalin@outlook.com>
2025-02-10 22:03:18 +0200

    lab 6 wip

---
+++ test/VehicleManagerTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VehicleManagerTest {
+
+    @Test
+    public void addVehicle() {
+    }
+
+    @Test
+    public void getVehicleSet() {
+    }
+}
